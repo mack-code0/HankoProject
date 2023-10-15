a977551a2530fc00ff7358f94528091716d50e6f
Macaulay Uzu <macdon202@gmail.com>
2023-10-15 18:20:39 +0100

    Fix: No note text center

--- src/components/NoNote.tsx
+++ src/components/NoNote.tsx
@@ -4,6 +4,6 @@ import EmptyImage from "../assets/images/empty-folder.png"
 export default function NoNote() {
     return <div className="w-full h-full flex flex-col items-center justify-center">
         <img className="opacity-50" src={EmptyImage} alt="" />
-        <h6 className="font-inter text-2xl font-semibold text-white text-opacity-50">No Note Yet! Please add a note</h6>
+        <h6 className="font-inter text-2xl font-semibold text-white text-center text-opacity-50">No Note Yet! Please add a note</h6>
     </div>
 }
